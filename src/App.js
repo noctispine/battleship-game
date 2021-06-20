d381142826877c9ce6de5dcad5089e04f205aac0
Eren Çam <pinefromw@gmail.com>
2021-06-21 00:07:09 +0300

    import body css

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import StateControl from "./StateControl";
 import GameContainer from "./components/gameWindow/GameContainer";
 import Header from "./components/header/Header";
-import { GameWrapper } from "./components/styled-components/boardStyles";
+import './App.css';
 
 function App() {
   return (
     <StateControl>
       <Header />
-      <GameWrapper>
         <GameContainer />
-      </GameWrapper>
     </StateControl>
   );
 }
